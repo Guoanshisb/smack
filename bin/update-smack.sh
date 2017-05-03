b6591fada194ca9e6f9a14a2351e1ff1ef880432
Guoanshisb <polarishehn@gmail.com>
2017-05-02 22:26:33 -0600

    Update bin/update-smack.sh

--- bin/update-smack.sh
+++ bin/update-smack.sh
@@ -1,3 +1,11 @@
 git checkout share/smack/svcomp/utils.py
 sed -i -- "s/recursionBound:65536/recursionBound:$1/g" share/smack/svcomp/utils.py
 sed -i -- "s/args\.unroll = 100/args\.unroll = $1/g" share/smack/svcomp/utils.py
+
+sed -i -- "s/INSTALL_DEPENDENCIES=1/INSTALL_DEPENDENCIES=0/g" bin/build.sh
+sed -i -- "s/BUILD_Z3=1/BUILD_Z3=0/g" bin/build.sh
+sed -i -- "s/BUILD_BOOGIE=1/BUILD_BOOGIE=0/g" bin/build.sh
+sed -i -- "s/BUILD_CORRAL=1/BUILD_CORRAL=0/g" bin/build.sh
+sed -i -- "s/BUILD_LOCKPWN=1/BUILD_LOCKPWN=0/g" bin/build.sh
+
+sudo bin/build.sh
